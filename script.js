a85403d79dc8c3cf946fe0e32112c8b8d29d968a
Fatim92 <fatimzhra1814@gmail.com>
2026-06-05 14:56:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let speedSlider = document.getElementById("speed");
+let freqText = document.getElementById("freq");
+let caseText = document.getElementById("caseText");
 
+function updateFrequency() {
+    let v = speedSlider.value;
+
+    // simulation simple Doppler
+    let freq = 100 + v * 10;
+
+    freqText.innerText = freq;
+}
+
+speedSlider.oninput = updateFrequency;
+updateFrequency();
+
+function normalCase() {
+    caseText.innerText = "Normal blood flow detected. No risk.";
+}
+
+function stenosisCase() {
+    caseText.innerText = "High velocity detected → possible arterial stenosis!";
+}
